483157aeb5ea87ff90e8c8bd31e2d48cf3e937af
MyLe <irisle412@gmail.com>
2023-04-30 20:23:20 +0700

    create type

---
+++ src/api/page-hero-banner/controllers/page-hero-banner.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * page-hero-banner controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::page-hero-banner.page-hero-banner');
